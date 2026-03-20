f4125b01e2a90ec717b0d08c288ba653602b6b33
shelbyshumard-cofc <shumards@cofc.edu>
2026-03-20 09:17:45 -0400

    Add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css';
 import 'leaflet/dist/leaflet.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
